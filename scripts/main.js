410672a2ccbf9b916d62710588468ae0f666f3dc
Plastel <110650329+Plastel@users.noreply.github.com>
2022-11-14 21:53:01 -0300

    make anthicus have extra weapon

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
 Planets.tantros.deafultCore = Vars.content.getByName(ContentType.block, "tantros-core-ruin")
+UnitTypes.anthicus.weapons.add(UnitTypes.cleroi.weapons.get(0))
+UnitTypes.anthicus.weapons.add(UnitTypes.cleroi.weapons.get(1))
+UnitTypes.anthicus.weapons.add(UnitTypes.cleroi.weapons.get(2))
+UnitTypes.anthicus.weapons.add(UnitTypes.cleroi.weapons.get(3))
