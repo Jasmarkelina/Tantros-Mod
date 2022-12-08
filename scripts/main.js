a32e4b2a2bb8fd2cea0ec8f29f9ffc5353fca375
Plastel <110650329+Plastel@users.noreply.github.com>
2022-12-08 19:50:34 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
-
+Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
+Planets.tantros.deafultCore = Vars.content.getByName(ContentType.block, "tantros-mod-core-ruin")
+Planets.tantros.hiddenItems = Items.erekirItems
