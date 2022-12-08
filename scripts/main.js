229bcf1fd0b421a559ae8c0160a7ff1dd7499f20
Jasmarkelina <111627777+Jasmarkelina@users.noreply.github.com>
2022-12-09 01:11:34 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,2 @@
 Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
-Planets.tantros.deafultCore = Vars.content.getByName(ContentType.block, "tantros-core-ruin")
+Planets.tantros.deafultCore = Vars.content.getByName(ContentType.block, "tantros-mod-core-ruin")
