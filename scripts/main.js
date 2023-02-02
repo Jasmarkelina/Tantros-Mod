a6287c00e91e398bbfa37c1b2fc9afad4cc88b7f
Plastel <110650329+Plastel@users.noreply.github.com>
2023-02-02 17:00:43 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,4 @@ Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.vi
 Planets.tantros.deafultCore = Vars.content.getByName(ContentType.block, "tantros-mod-core-ruin")
 Planets.tantros.hiddenItems = Items.erekirItems
 Planets.tantros.techtree = techtree.getByName(ContentType.block, "tantros-mod-core-ruin")
+UnitTypes.cleroi.weapons.each(w => UnitTypes.anthicus.weapons.add(w));
