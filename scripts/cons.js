71939e3da68c79fa80eaa40e2c0b4c1bbbae2248
RouterXdd <91018607+RouterXdd@users.noreply.github.com>
2023-09-11 19:19:04 +0300

    Save

--- scripts/cons.js
+++ scripts/cons.js
@@ -2,4 +2,5 @@ Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.vi
 Planets.tantros.hiddenItems = Items.erekirItems
 Planets.tantros.allowLaunchToNumbered = false
 Planets.tantros.allowLaunchLoadout = true
+Planets.tantros.rules.loadout = ItemStack.list(Vars.content.item("tantros-mod-Nickel"), 200)
 UnitTypes.cleroi.weapons.each(w => UnitTypes.anthicus.weapons.add(w))
