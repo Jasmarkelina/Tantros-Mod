59f9d3fe16b4912f7b0de230fa627201fe34238f
RouterXdd <91018607+RouterXdd@users.noreply.github.com>
2023-09-15 07:10:33 +0300

    Ornicus change

--- scripts/cons.js
+++ scripts/cons.js
@@ -1,6 +1,8 @@
+Events.run(ClientLoadEvent, () => {
 Planets.tantros.accessible = Planets.tantros.alwaysUnlocked = Planets.tantros.visible = true
 Planets.tantros.hiddenItems = Items.erekirItems
 Planets.tantros.allowLaunchToNumbered = false
 Planets.tantros.allowLaunchLoadout = true
-Planets.tantros.rules.loadout = ItemStack.list(Vars.content.item("tantros-mod-Nickel"), 200)
+Planets.tantros.allowLaunchSchematics = true
 UnitTypes.cleroi.weapons.each(w => UnitTypes.anthicus.weapons.add(w))
+})
